22d563862248031d49c3530546b6a48644d01fd3
Sumanth Krishna P <76647205+a55kickr947@users.noreply.github.com>
2023-08-23 22:45:40 +0530

    Update scipt.js

--- webpage/scipt.js
+++ webpage/scipt.js
@@ -1,25 +1,41 @@
-const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
-const header = document.querySelector('.header.container');
-
-hamburger.addEventListener('click', () => {
-	hamburger.classList.toggle('active');
-	mobile_menu.classList.toggle('active');
-});
-
-document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
+document.addEventListener("DOMContentLoaded", function () {
+	const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+	const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+	const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+	const header = document.querySelector('.header.container');
+  
+	hamburger.addEventListener('click', () => {
+	  hamburger.classList.toggle('active');
+	  mobile_menu.classList.toggle('active');
+	});
+  
+	document.addEventListener('scroll', () => {
+	  var scroll_position = window.scrollY;
+	  if (scroll_position > 250) {
 		header.style.backgroundColor = '#29323c';
-	} else {
+	  } else {
 		header.style.backgroundColor = 'transparent';
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener('click', () => {
+	  }
+	});
+  
+	menu_item.forEach((item) => {
+	  item.addEventListener('click', () => {
 		hamburger.classList.toggle('active');
 		mobile_menu.classList.toggle('active');
+	  });
 	});
-});
+  
+	menu_item.forEach((link) => {
+	  link.addEventListener("click", (e) => {
+		e.preventDefault();
+		const target = document.querySelector(e.target.getAttribute("href"));
+		if (target) {
+		  window.scrollTo({
+			top: target.offsetTop,
+			behavior: "smooth",
+		  });
+		}
+	  });
+	});
+  });
+  
